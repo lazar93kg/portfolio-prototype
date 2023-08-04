dc0b5b2b77852b774f7536695e4826ed66730f20
lazar93kg <lazar93kg@gmail.com>
2023-08-04 16:26:43 +0200

    Update

--- components/About.jsx
+++ components/About.jsx
@@ -21,7 +21,7 @@ function About() {
           </p>
         </div>
         <div className={styles.infoAbout2}>
-          <span>8+</span>
+          <span>7+</span>
           <p>
             Months of <br /> Internship
           </p>
@@ -40,20 +40,40 @@ function About() {
         <p>
           <br />
           My journey as a front-end developer started with a solid foundation,
-          having pursued formal education for two years in the field. Throughout
-          my academic years, I delved into various programming languages,
-          front-end frameworks, and responsive design principles, empowering me
-          to create visually stunning and user-friendly websites.
+          having pursued formal education for two years in the field.
           <br />
           <br />
-          Following my academic pursuits, I embarked on an enriching internship
-          at Devinova AB that has further enriched my expertise. For more than
-          seven months, I've had the incredible opportunity to collaborate with
-          talented professionals, working on real-world projects that challenged
-          and inspired me. This invaluable experience at Devinova AB has not
-          only strengthened my technical abilities but also taught me the value
-          of effective teamwork and adaptability in the fast-paced tech
-          industry.
+          During my academic years, I immersed myself in a diverse array of
+          programming languages, from the essentials like{" "}
+          <strong style={{ fontWeight: "600" }}>
+            HTML, CSS, and JavaScript
+          </strong>{" "}
+          to cutting-edge technologies such as{" "}
+          <strong style={{ fontWeight: "600" }}>
+            TypeScript, Node, React, MongoDB, Firebase
+          </strong>
+          , and responsive design principles. This well-rounded education has
+          empowered me to craft visually stunning and user-friendly websites
+          that leave a lasting impression.
+          <br />
+          <br />
+          Eager to put my knowledge into practice, I embarked on an enriching
+          internship at Devinova AB, where I truly honed my skills and evolved
+          as a developer. For more than seven months, I've had the incredible
+          opportunity to collaborate with talented professionals, working on
+          real-world projects that challenged and inspired me.
+          <br />
+          <br />
+          During my time at Devinova AB, we leveraged an exciting tech stack,
+          including{" "}
+          <strong style={{ fontWeight: "600" }}>
+            Gatsby, Next, Theme-UI, Strapi, and GraphQL{" "}
+          </strong>
+          , skillfully deploying our creations on platforms like{" "}
+          <strong style={{ fontWeight: "600" }}>Heroku and Vercel</strong>. This
+          invaluable experience not only strengthened my technical abilities but
+          also instilled in me the value of effective teamwork and adaptability
+          in the fast-paced tech industry.
         </p>
       </div>
     </section>
--- data/skillsData.js
+++ data/skillsData.js
@@ -2,50 +2,50 @@ export const frontEnd = [
   {
     id: "001",
     technicalSkills: "HTML",
-    percentage: "92%",
+    percentage: "99%",
   },
   {
     id: "002",
     technicalSkills: "CSS / Theme-UI",
-    percentage: "90%",
+    percentage: "99%",
   },
   {
     id: "003",
     technicalSkills: "JavaScript /TypeScript",
-    percentage: "75%",
+    percentage: "90%",
   },
 ];
 export const library = [
   {
     id: "001",
     technicalSkills: "React.js",
-    percentage: "71%",
+    percentage: "90%",
   },
   {
     id: "002",
     technicalSkills: "Next.js",
-    percentage: "72%",
+    percentage: "90%",
   },
   {
     id: "009",
     technicalSkills: "Gatsby.js",
-    percentage: "73%",
+    percentage: "90%",
   },
 ];
 export const backend = [
   {
     id: "001",
     technicalSkills: "CMS Strapi / GraphQL",
-    percentage: "75%",
+    percentage: "90%",
   },
   {
     id: "003",
     technicalSkills: "Node.js",
-    percentage: "60%",
+    percentage: "80%",
   },
   {
     id: "005",
     technicalSkills: "MongoDB / Firebase",
-    percentage: "30%",
+    percentage: "40%",
   },
 ];
